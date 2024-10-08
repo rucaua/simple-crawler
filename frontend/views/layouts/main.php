85b8509c5e5707ba966dc28af23da8bd4fa6db02
Maksym Tymofeiev <72993951+rucaua@users.noreply.github.com>
2024-10-07 21:01:29 -0600

    Update layout to use container-fluid with consistent padding

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -30,24 +30,17 @@ AppAsset::register($this);
     NavBar::begin([
         'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
+        'innerContainerOptions' => ['class' => 'container-fluid px-5'],
         'options' => [
             'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
         ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-    ];
-
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
-        'items' => $menuItems,
     ]);
     NavBar::end();
     ?>
 </header>
 
 <main role="main" class="flex-shrink-0">
-    <div class="container-fluid p-5 overflow-auto">
+    <div class="container-fluid px-5 overflow-auto">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
@@ -57,7 +50,7 @@ AppAsset::register($this);
 </main>
 
 <footer class="footer mt-auto py-3 text-muted">
-    <div class="container">
+    <div class="container-fluid px-5">
         <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
         <p class="float-end"><?= Yii::powered() ?></p>
     </div>
